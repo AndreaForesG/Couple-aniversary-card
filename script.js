56837460b6f49a53a280cffd3c23b68f378c07ae
AndreaForesG <andreafores@gmail.com>
2024-09-09 20:31:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarMensaje() {
+    const mensaje = document.getElementById('mensaje');
+    mensaje.innerHTML = "<p>Eres la persona más especial en mi vida.</p>";
+    mensaje.innerHTML += "<p>El principio de una vida juntos, por muchos aniversarios más.</p>";
+    mensaje.innerHTML += "<p>Te amo con todo mi corazón 💖</p>";
+}
